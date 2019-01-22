e3d87e80df8e5f5c583cac2ff9a6cc975b4bfefa
Alan Drieux <alan.drieux@outlook.fr>
2019-01-22 11:38:57 +0100

    Finish story 9 , Trasnform Decimal value to Base64 value caractere

--- base64_ad.py
+++ base64_ad.py
@@ -9,9 +9,11 @@ def binaire_to_decimal(binaire):
     """ Transform binaire value to decimal value
 
         Args:
-          binaire: value binaire.
+            binaire(list): list of the value binaire.
+
         Returns:
-          List decimal.
+            List: decimal.
+
     """
 
     for index, number in enumerate(binaire):
@@ -24,3 +26,29 @@ def binaire_to_decimal(binaire):
 binaire_to_decimal(["010000","010100","001001","000011","010001","000000"])
 
 # resultat = [16, 20, 9, 3, 17, 0]
+
+
+# Parti 9 : Transformer chaque element de la liste dans ca valeur decimal
+# Exemple : [16, 20, 9, 3, 17, 0]
+
+base64_caractere = list("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/")
+
+def decimal_to_base64(transformList):
+    """ Transform ecimal value to base64 caractere
+
+        Args:
+            transformList(List): list of the value decimal.
+
+        Returns:
+            List: String value.
+
+    """
+    for index, number in enumerate(transformList):
+        transformList[index] = base64_caractere[transformList[index]]
+
+    print(transformList)
+    return transformList
+
+decimal_to_base64([16, 20, 9, 3, 17, 0])
+
+# resultat = ["Q", "U", "J", "D", "R", "A"]
