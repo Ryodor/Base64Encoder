b089b7daf30482ade0045c9ccd34c42edee66cb8
BounaixVincent <bounaixvincent@gmail.com>
2019-01-22 11:00:19 +0100

    story 3 done

--- BV.py
+++ BV.py
@@ -1,13 +1,16 @@
 
+
+
 # This method returns a string with 'ABCD' in value
 
 
 def return_char_string():
     char_string = 'ABCD'
     print(char_string)
+    return(char_string)
 
 
-return_char_string()
+char_string = return_char_string()
 
 
 # This method transforms a string into a list filled with every character of the string
@@ -15,10 +18,24 @@ return_char_string()
 def transform_to_list(char_str):
     chars = []
     for character in char_str:
-        for c in character:
-            chars.append(c)
+            chars.append(character)
 
     print(chars)
+    return(chars)
 
 
-transform_to_list('ABCD')
+char_list = transform_to_list(char_string)
+
+
+# This method transforms each element in its ASCII code
+
+def transform_to_ascii(list):
+    ascii_list = []
+    for character in list:
+            ord(character)
+            ascii_list.append(ord(character))
+
+    print(ascii_list)
+    return(ascii_list)
+
+transform_to_ascii(char_list)
