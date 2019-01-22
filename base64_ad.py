b77621232ac68a3e8c07a83fe44f4e55bbe456a4
Alan Drieux <alan.drieux@outlook.fr>
2019-01-22 10:35:30 +0100

    Create file base§'_ad.py

---
+++ base64_ad.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Parti 8 : Transformer chaque element de la liste dans ca valeur decimal
+# Exemple : ["010000","010100","001001","000011","010001","000000"]
+
