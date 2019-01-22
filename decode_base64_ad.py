31d6561af7503a7efdfdcab6c233103d88f17d14
Alan Drieux <alan.drieux@outlook.fr>
2019-01-22 16:44:06 +0100

    Finish story 19 : Transfrom

--- decode_base64_ad.py
+++ decode_base64_ad.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Story 17: Transformer chaque élément de la liste dans sa représentation binaire (cadrée à droite sur 6 positions)
+# Story 17: Transformer chaque élément de la liste
+# dans sa représentation binaire (cadrée à droite sur 6 positions)
 # Exemple : [16, 20, 9, 3, 17, 0]
 
 
@@ -33,6 +34,7 @@ b64_decimal_to_b64_binary([16, 20, 9, 3, 17, 0])
 # Story 18: Transformer la liste en une chaine de caractères
 # Exemple : ["010000","010100","001001","000011","010001","000000"]
 
+
 def binary_to_string(tranfrom_value):
     """ Tranform binary value list to string binary value
 
@@ -48,4 +50,30 @@ def binary_to_string(tranfrom_value):
 
 binary_to_string(["010000","010100","001001","000011","010001","000000"])
 
-#Result : "010000010100001001000011010001000000"
+# Result : "010000010100001001000011010001000000"
+
+# Story 19: Raccourcir la chaine (par la droite) pour que sa longueur,
+# soit multiple de huit.
+# Exemple : "010000010100001001000011010001000000"
+
+
+def transfrom_binary_multipl8(transform_binary):
+    """ Trasnfrom the string binary so that size of the string,
+        be equal to a multpiple of 8
+
+    Args:
+        transform_binary(String): String of binary value
+
+    Returns: string binary value equal to o a multpiple of 8
+
+    """
+
+    if (len(transform_binary) % 8) > 0:
+        rest = len(transform_binary) % 8
+        transform_binary = transform_binary[:-rest]
+
+    return transform_binary
+
+transfrom_binary_multipl8("010000010100001001000011010001000000")
+
+# Result : "01000001010000100100001101000100"
