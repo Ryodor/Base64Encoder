c5e4143916e9e58f302a8e24d7554f651a399beb
BounaixVincent <bounaixvincent@gmail.com>
2019-01-22 11:28:17 +0100

    recommit becaused I screwed up

--- BV.py
+++ BV.py
@@ -1,10 +1,11 @@
-
-
-
-# This method returns a string with 'ABCD' in value
-
-
 def return_char_string():
+    """This method returns a string with 'ABCD' in value
+
+    :return:
+        string: always equal to 'ABCD'
+    """
+
+
     char_string = 'ABCD'
     print(char_string)
     return(char_string)
@@ -16,6 +17,12 @@ char_string = return_char_string()
 # This method transforms a string into a list filled with every character of the string
 
 def transform_to_list(char_str):
+    """
+
+    :param
+        char_str:
+    :return:
+    """
     chars = []
     for character in char_str:
             chars.append(character)
@@ -29,13 +36,29 @@ char_list = transform_to_list(char_string)
 
 # This method transforms each element in its ASCII code
 
-def transform_to_ascii(list):
+def transform_to_ascii(char_list):
     ascii_list = []
-    for character in list:
+    for character in char_list:
             ord(character)
             ascii_list.append(ord(character))
 
     print(ascii_list)
     return(ascii_list)
 
-transform_to_ascii(char_list)
+
+ascii_list = transform_to_ascii(char_list)
+
+
+# This method transforms each element in its binary value
+
+def transform_to_binary(ascii_list):
+    binary_list = []
+    for element in ascii_list:
+        i = bin(element)
+        binary_list.append(i)
+
+    print(binary_list)
+    return(binary_list)
+
+
+transform_to_binary(ascii_list)
